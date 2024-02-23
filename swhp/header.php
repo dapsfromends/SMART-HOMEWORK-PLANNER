9266a1adaaf51dcd1006f0bb6c72abc4a051cb69
toluwatobi <111466241+toluwatobi@users.noreply.github.com>
2024-02-23 14:57:26 +0000

    some

---
+++ swhp/header.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Smart Homework Planner </title>
+    <meta name="viewport" contenteditable="widdth=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="styles.css">
+    <link rel="stylesheet" href="unsemantic.css">
+    <title>Smart Homework Planner </title>
+</head>
+
+<body>
+        <nav>
+            <ul>
+                <li><a href="login.php">Login</a></li>
+                <li><a href="signup.html"> Sign up</a></li>
+                <li><a href="logout.php">Logout</a></li>
+            </ul>
+        </nav>
+    
+        <div class = "blanket">
